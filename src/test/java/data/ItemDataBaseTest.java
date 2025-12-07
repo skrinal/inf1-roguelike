b0550c93661806224ae55801c037417f4cab7318
SkrinalAI <richard.kamenistak@gmail.com>
2025-12-07 19:05:02 +0100

    Refactoring name

--- src/test/java/data/ItemDataBaseTest.java
+++ src/test/java/data/ItemDataBaseTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-class ItemDatabaseTest {
+class ItemDataBaseTest {
 
     private Player magePlayer;
     private Player roguePlayer;
