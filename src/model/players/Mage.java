8b83f6bd00e104578f2bbe01854fabf524349e4d
SkrinalAI <richard.kamenistak@gmail.com>
2025-12-15 17:23:57 +0100

    Mage ability

--- src/model/players/Mage.java
+++ src/model/players/Mage.java
@@ -1,16 +1,24 @@
 package model.players;
 
 import model.Character;
+import model.Enemy;
 import model.Player;
 import model.enums.ClassPower;
 import model.enums.PlayerClass;
 import model.enums.status.StatusEffects;
+
+import java.util.Random;
 
 public class Mage extends Player {
     private static final int MAX_HP = 80;
     private static final int ATTACK = 10;
     private static final int DEFENCE = 3;
     private static final int POWER = 100;
+
+    private double invisibilityChance = 0.4;
+    private boolean isInvisible = false;
+
+    private final Random random = new Random();
 
     public Mage(String name) {
         super(name, MAX_HP, ATTACK, DEFENCE, POWER);
@@ -54,17 +62,42 @@ public class Mage extends Player {
 
     @Override
     public void performeUtilityAbitlity() {
-        if (usePower(15)) {
-            this.applyStatusEffect(StatusEffects.INVISIBILITY, -1);
-            System.out.println("You have used !");
+        if (!this.isInvisible) {
+            if (this.usePower(15)) {
+                this.applyStatusEffect(StatusEffects.INVISIBILITY, -1);
+                System.out.println("You have used !");
+            } else {
+                System.out.println("Not enough Mana!");
+            }
         } else {
-            System.out.println("Not enough Mana!");
+            System.out.println("You are already invisible!");
         }
     }
 
     @Override
     public void beforeTurn() {
+        if (!this.isInvisible) {
+            this.restorePower(5);
+            this.checkInvisibilityStatus();
+
+        }
         restorePower(10);
+    }
+
+    private void checkInvisibilityStatus() {
+        if (this.isInvisible) {
+            if (this.random.nextDouble() >= this.invisibilityChance) {
+
+                System.out.println("You remain invisible!");
+                this.invisibilityChance -= 0.1;
+            } else {
+                System.out.println("You are no longer invisible!");
+
+                this.removeStatusEffect(StatusEffects.INVISIBILITY);
+                this.isInvisible = false;
+                this.invisibilityChance = 0.5;
+            }
+        }
     }
 
 
