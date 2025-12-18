bafdd13bdbd98933d1ae16b3e86d9dba431702e7
Richard Kamenišťak <richard.kamenistak@gmail.com>
2025-12-18 01:31:16 +0100

    Type of mob enum

--- src/main/java/model/enums/EnemyType.java
+++
@@ -1,9 +0,0 @@
-package model.enums;
-
-public enum EnemyType {
-    SKELETON,
-    TROLL,
-    ELF,
-    DRAGON,
-    DEMON
-}
---
+++ src/main/java/model/enums/status/EnemyType.java
@@ -0,0 +1,7 @@
+package model.enums.status;
+
+public enum EnemyType {
+    TRASH,
+    ELITE,
+    BOSS
+}
