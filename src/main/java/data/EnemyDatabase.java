f1f8c821a9de28b4a1b337fbcf94ea8619bf5e66
Richard Kamenišťak <richard.kamenistak@gmail.com>
2026-01-04 01:31:51 +0100

    Changed the room enemy layouts

--- src/main/java/data/EnemyDatabase.java
+++ src/main/java/data/EnemyDatabase.java
@@ -1,6 +1,10 @@
 package data;
 
 import model.Enemy;
+import model.enemies.DemonLord;
+import model.enemies.Dragon;
+import model.enemies.Elf;
+import model.enemies.Troll;
 import model.enemies.Skeleton;
 import model.enums.room.RoomType;
 
@@ -9,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EnemyDatabase {
-    private Map<RoomType, ArrayList<Enemy>> enemies;
+    private final Map<RoomType, ArrayList<Enemy>> enemies;
     private static EnemyDatabase instance;
 
     public static EnemyDatabase getInstance() {
@@ -26,16 +30,32 @@ public class EnemyDatabase {
     }
 
     private void loadEnemies() {
+        //TODO: change Names
         ArrayList<Enemy> roomOneEnemies = new ArrayList<>();
         roomOneEnemies.add(new Skeleton("Maximus"));
-        roomOneEnemies.add(new Skeleton("Bonecrusher"));
 
         ArrayList<Enemy> roomTwoEnemies = new ArrayList<>();
-        roomTwoEnemies.add(new Skeleton("Maximus"));
+        roomTwoEnemies.add(new Skeleton("Rotting Guard"));
         roomTwoEnemies.add(new Skeleton("Bonecrusher"));
+        roomTwoEnemies.add(new Troll("Cave Troll"));
+
+        ArrayList<Enemy> roomThreeEnemies = new ArrayList<>();
+        roomTwoEnemies.add(new Elf("Elite Guard"));
+
+
+        ArrayList<Enemy> roomFourEnemies = new ArrayList<>();
+        roomTwoEnemies.add(new Dragon("Gatekeeper"));
+
+
+        ArrayList<Enemy> roomFiveEnemies = new ArrayList<>();
+        roomTwoEnemies.add(new DemonLord("The Ashen Warden"));
+
 
         this.enemies.put(RoomType.ONE, roomOneEnemies);
         this.enemies.put(RoomType.TWO, roomTwoEnemies);
+        this.enemies.put(RoomType.THREE, roomThreeEnemies);
+        this.enemies.put(RoomType.FOUR, roomFourEnemies);
+        this.enemies.put(RoomType.FIVE, roomFiveEnemies);
     }
 
     public ArrayList<Enemy> getEnemies(RoomType roomType) {
